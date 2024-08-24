5f3f8864cb24ca95e22a063b2e5f6e0ab0b43329
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-08-24 01:47:31 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,4 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
+app.get("/", function (req, res) {
+  //   res.send("Hello Express");
+  const absolutePath = path.join(__dirname, `/views/index.html`);
+  res.sendFile(absolutePath);
+});
+
 module.exports = app;
